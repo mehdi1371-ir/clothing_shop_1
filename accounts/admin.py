d1167894935b8ba5ccece7b50140de42ae69cab4
mehdi pourbaba <mhdpourbaba@gmail.com>
2022-05-27 16:18:15 +0430

    add CustoUserAdmin to accounts.admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import CustomUser
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    list_display = ['username', 'email', 'is_staff']
+    fieldsets = UserAdmin.fieldsets
+    add_fieldsets = UserAdmin.add_fieldsets
