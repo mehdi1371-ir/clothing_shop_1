ece89c9d15d92cc0a420dbf5ba2eaedbfaa921d2
mehdi pourbaba <mhdpourbaba@gmail.com>
2022-05-28 22:12:14 +0430

    about page tests added

--- pages/tests.py
+++ pages/tests.py
@@ -12,3 +12,13 @@ class HomePageTest(TestCase):
     def test_home_page_url_with_name(self):
         response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
+
+class AboutPageTest(TestCase):
+    
+    def test_about_page_url(self):
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_page_url_by_name(self):
+        response = self.client.get(reverse('about'))
+        self.assertEqual(response.status_code, 200)
