795d616ff99e9555f66e8089adb8c85bf5f604e2
mehdi pourbaba <mhdpourbaba@gmail.com>
2022-05-28 21:16:42 +0430

    about page views and urls added

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+
+from .models import AboutUs, OurTeam, OurService
 
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+
+class AboutPageView(ListView):
+    model = AboutUs
+    template_name = 'pages/about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(AboutPageView, self).get_context_data(**kwargs)
+        context['abouts'] = AboutUs.objects.all()
+        context["teams"] = OurTeam.objects.all()
+        context['services'] = OurService.objects.all()
+        return context
+
+    
