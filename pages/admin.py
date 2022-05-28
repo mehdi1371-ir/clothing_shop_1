c891d1e7f007b30771fae986747c3750a0f945af
mehdi pourbaba <mhdpourbaba@gmail.com>
2022-05-28 20:41:29 +0430

    about page admins added

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import AboutUs, OurTeam, OurService
+
+
+class OurTeamInline(admin.StackedInline):
+    model = OurTeam
+    extra = 0
+    classes = ['collapse']
+
+
+class OurServiceInline(admin.StackedInline):
+    model = OurService
+    extra = 0
+    classes = ['collapse']
+
+
+@admin.register(AboutUs)
+class AboutUsAdmin(admin.ModelAdmin):
+    inlines = [OurTeamInline, OurServiceInline]
