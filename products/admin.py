a1397b967b9633d646d00c1cf21952294a94f045
mehdi pourbaba <mhdpourbaba@gmail.com>
2022-05-29 23:21:08 +0430

    product admin added

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Product, Image
+
+
+admin.site.register(Category)
+
+
+class ImageInline(admin.StackedInline):
+    model = Image
+    extra = 0
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ImageInline]
