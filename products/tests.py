c50f37913f2f9cd9ab605950e9acb0c531972507
mehdi pourbaba <mhdpourbaba@gmail.com>
2022-06-13 21:31:19 +0430

    product objects entrance test added

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,5 @@
+from email.mime import image
+import tempfile
 from django.test import TestCase
 from django.core.files.uploadedfile import SimpleUploadedFile
 
@@ -8,16 +10,27 @@ class ProductTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.category = Category.objects.create(name='mens')
+        cls.image1 = tempfile.NamedTemporaryFile(suffix=".jpg").name
+        cls.image2 = tempfile.NamedTemporaryFile(suffix=".jpg").name
         cls.product = Product.objects.create(
             title = 'product1',
             slug = 'product_1',
             description = 'description of product1',
-            image1 = SimpleUploadedFile(name='test_image.jpg', content=open('media/products/men-02_hjzZU1E.jpg', 'rb').read(), content_type='image/jpeg'),
-            image2 = SimpleUploadedFile(name='test_image.jpg', content=open('media/products/men-02_vHkUNdj.jpg', 'rb').read(), content_type='image/jpeg'),
             price = 25.55,
+            image1 = cls.image1,
+            image2 = cls.image2,
             category = cls.category,
         )
 
     def test_product_model_str(self):
         product = self.product
         self.assertEqual(str(product), product.title)
+
+    def test_product_objects(self):
+        self.assertEqual(self.product.title, 'product1')
+        self.assertEqual(self.product.description, 'description of product1')
+        self.assertEqual(self.product.slug, 'product_1')
+        self.assertEqual(self.product.price, 25.55)
+        self.assertEqual(self.product.image1, self.image1)
+        self.assertEqual(self.product.image2, self.image2)
+        self.assertEqual(self.product.category, self.category)
