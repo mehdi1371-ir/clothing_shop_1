ea4ddb4a0d13d00f57045a61fadebda825a4b157
mehdi pourbaba <mhdpourbaba@gmail.com>
2022-07-02 15:05:41 +0430

    add smtp settings

--- config/settings.py
+++ config/settings.py
@@ -170,3 +170,9 @@ MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
 
 # Email config
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_USE_TLS = True
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = '???'
+# EMAIL_HOST_PASSWORD = '????'
