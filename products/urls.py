4a45707fdef606e5a1663aef358e7bc1905ccd2b
mehdi pourbaba <mhdpourbaba@gmail.com>
2022-07-02 16:13:30 +0430

    change category view to functional

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .views import ProductListView, ProductDetailView, CategoryListView
+from .views import ProductListView, ProductDetailView, category
 
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='products_list'),
-    path('<slug:slug>/', CategoryListView.as_view(), name='category'),
+    path('<slug:slug>/', category, name='category'),
     path('detail/<slug:slug>/', ProductDetailView.as_view(), name='product_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render
 from django.views.generic import ListView, DetailView
 
 from .models import Product, Category
@@ -16,16 +16,27 @@ class ProductDetailView(DetailView):
     context_object_name = 'product'
     
 
-class CategoryListView(ListView):
-    template_name = 'products/category_detail.html'
+# class CategoryListView(ListView):
+#     template_name = 'products/category_detail.html'
 
-    def get_queryset(self):
-        category = get_object_or_404(Category, slug=self.kwargs['slug'])
-        self.products = category.products.all()
-        return self.products
+#     def get_queryset(self):
+#         category = get_object_or_404(Category, slug=self.kwargs['slug'])
+#         self.products = category.products.all()
+#         return self.products
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["products"] = self.products
-        return context
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context["products"] = self.products
+#         return context
     
+
+def category(request, slug):
+    category = Category.objects.get(slug=slug)
+    cat_products = category.products.all()
+
+    context = {
+        'category': category,
+        'cat_products': cat_products
+    }
+
+    return render(request, 'products/category_detail.html', context)
